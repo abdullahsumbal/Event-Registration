c18d7c8d0bf7af3d039244946cb64139cd6a76d0
abdullahsumbal <abdullahsumbal@gmail.com>
2019-11-13 16:45:22 -0500

    template

---
+++ back-end/app.py
@@ -0,0 +1,65 @@
+#!flask/bin/python
+from flask import Flask, jsonify, abort, make_response, request, url_for
+
+app = Flask(__name__)
+
+events = [
+    {
+        'id': 1,
+        'name': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
+        'start date' : 'date' 
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python tutorial on the web', 
+        'done': False
+    }
+]
+
+
+# by default flask sends the html 404 page. 
+# I want to send a json with 404 error
+# to be consist with my other returns
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+def make_public_task(task):
+    new_task = {}
+    for field in task:
+        if field == 'id':
+            new_task['uri'] = url_for('get_task', task_id=task['id'], _external=True)
+        else:
+            new_task[field] = task[field]
+    return new_task
+
+@app.route('/api/events', methods=['GET'])
+def get_events():
+    return jsonify({'tasks': [make_public_task(task) for task in tasks]})
+
+
+@app.route('/api/events/<int:task_id>', methods=['GET'])
+def get_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+    return jsonify({'task': task[0]})
+
+@app.route('/todo/api/v1.0/tasks', methods=['POST'])
+def create_task():
+    if not request.json or not 'title' in request.json:
+        abort(400)
+    task = {
+        'id': tasks[-1]['id'] + 1,
+        'title': request.json['title'],
+        'description': request.json.get('description', ""),
+        'done': False
+    }
+    tasks.append(task)
+    return jsonify({'task': task}), 201
+
+if __name__ == '__main__':
+    app.run(debug=True)
