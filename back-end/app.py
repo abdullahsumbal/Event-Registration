0a8e3de4348abe02be0a79e209afe8222388c19b
abdullahsumbal <abdullahsumbal@gmail.com>
2019-11-14 03:53:14 -0500

    connected to database , now respondes to frontend

--- back-end/app.py
+++ back-end/app.py
@@ -1,56 +1,79 @@
 #!flask/bin/python
+import sys
+import psycopg2
+from database import connect_db
 from flask import Flask, jsonify, abort, make_response, request, url_for
 
 app = Flask(__name__)
 
-events = [
-    {
-        'id': 1,
-        'name': u'Buy groceries',
-        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
-        'start date' : 'date' 
-        'done': False
-    },
-    {
-        'id': 2,
-        'title': u'Learn Python',
-        'description': u'Need to find a good Python tutorial on the web', 
-        'done': False
-    }
-]
+CORS(app)
 
+# global object Flask uses for passing information to views/modules.
+db = {'connect': None}
+
+########################################################
+#             Before and After request            
+########################################################
+
+@app.before_request
+def before_request():
+    """Connect to the database before each request."""
+    db['connect'] = connect_db()
+
+
+@app.after_request
+def after_request(response):
+    """Close the database connection after each request."""
+    if db['connect']:
+        db['connect'].close()
+    return response
+
+########################################################
+#                   Error Handler            
+########################################################
 
 # by default flask sends the html 404 page. 
 # I want to send a json with 404 error
 # to be consist with my other returns
 @app.errorhandler(404)
 def not_found(error):
+    """ Error handler for 404 error"""
     return make_response(jsonify({'error': 'Not found'}), 404)
 
-def make_public_task(task):
-    new_task = {}
-    for field in task:
-        if field == 'id':
-            new_task['uri'] = url_for('get_task', task_id=task['id'], _external=True)
-        else:
-            new_task[field] = task[field]
-    return new_task
+@app.errorhandler(503)
+def service_down(error):
+    """ Error handler for 503 error. Used for database"""
+    return make_response(jsonify({'error': 'Unable to Connect to Database'}), 503)
+
+########################################################
+#                   Event Requests            
+########################################################
 
 @app.route('/api/events', methods=['GET'])
 def get_events():
-    return jsonify({'tasks': [make_public_task(task) for task in tasks]})
-
-
-@app.route('/api/events/<int:task_id>', methods=['GET'])
-def get_task(task_id):
-    task = [task for task in tasks if task['id'] == task_id]
-    if len(task) == 0:
+    """Get all the events"""
+    cur = get_cursor()
+    cur.execute("select EventId, EventName from Events")
+    events = add_descrption(cur.fetchall(), cur.description)
+    if len(events) == 0:
         abort(404)
-    return jsonify({'task': task[0]})
+    return jsonify({'events': events})
+
+
+@app.route('/api/events/<int:event_id>', methods=['GET'])
+def get_task(event_id):
+    """ Get event given id"""
+    cur = get_cursor()
+    cur.execute("select * from Events where eventid = %s", [event_id])
+    event = add_descrption(cur.fetchall(), cur.description)
+    if len(event) == 0:
+        abort(404)
+    return jsonify({'event': event})
+
 
 @app.route('/todo/api/v1.0/tasks', methods=['POST'])
 def create_task():
-    if not request.json or not 'title' in request.json:
+    if not request.json or 'title' in request.json:
         abort(400)
     task = {
         'id': tasks[-1]['id'] + 1,
@@ -60,6 +83,28 @@ def create_task():
     }
     tasks.append(task)
     return jsonify({'task': task}), 201
+
+########################################################
+#                   helper function          
+########################################################
+
+def get_cursor():
+    return db['connect'].cursor() if db['connect'] else abort(503)
+
+def add_descrption(rows, discription):
+    """ add column name to results"""
+    response = []
+    for row in rows:
+        new_row = {}
+        for col_data, col_value in zip(discription, row):
+            col_name = col_data[0]
+            if 'eventid' in col_name:
+                new_row['uri'] = url_for('get_task', event_id=col_value, _external=True)
+            else:
+                new_row[col_name] = col_value
+        response.append(new_row)
+    return response
+
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ back-end/database.py
@@ -0,0 +1,17 @@
+import psycopg2
+from config import config
+
+def connect_db():
+    """ Connect to the PostgreSQL database server """
+    try:
+        # read connection parameters
+        params = config()
+ 
+        # connect to the PostgreSQL server
+        print('Connecting to the PostgreSQL database...')
+        conn = psycopg2.connect(**params)
+
+        return conn
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+        return None
